3003a5af256cc4db6c8ea21237d4086c46a2ba1b
도예 <kmdy125@gmail.com>
2023-05-16 11:26:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+
+st.title("최고의 수업, oss 개발에서 배운 Streamlit")
+df = pd.DataFrame(np.random.randn(500,2) / [50, 50] + [37.76, -122.4], colums=['lar', 'lon'])
+st.map(df)
